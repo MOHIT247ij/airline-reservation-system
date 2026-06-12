17c8537fbe1f78488741704b4c8f5fb99b1e8889
MOHIT247ij <mohithingase04@gmail.com>
2026-06-12 16:30:14 +0530

    Updated Firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYAxNtVcsOnA6jr0HgrsxwO2q1erEywns",
-  authDomain: "airline-reservations-sys-e4743.firebaseapp.com",
-  projectId: "airline-reservations-sys-e4743",
-  storageBucket: "airline-reservations-sys-e4743.firebasestorage.app",
-  messagingSenderId: "261299922673",
-  appId: "1:261299922673:web:b3959729f6788e89816b50",
-  measurementId: "G-EF3HBM85CG"
+  apiKey: "AIzaSyCgX6V5vteVEzOWqyAHdrY0jHCnvpm_Skc",
+  authDomain: "dbms-project-daa39.firebaseapp.com",
+  projectId: "dbms-project-daa39",
+  storageBucket: "dbms-project-daa39.firebasestorage.app",
+  messagingSenderId: "331538039093",
+  appId: "1:331538039093:web:00663e22745dc774100c7c",
+  measurementId: "G-KN8YJGEP46"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
